8ab74149b2e744521c12bfff4e8c50a65996904f
meayang <skaru2002@naver.com>
2021-12-19 18:07:16 +0900

    socketio install

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import http from "http";
-import WebSocket from "ws";
+import socketIO from "socket.io";
 import express from "express";
 
 const app = express();
@@ -10,34 +10,60 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
+const httpServer = http.createServer(app);
+const wsServer = socketIO(httpServer);
 
-const server = http.createServer(app);
-const wss = new WebSocket.Server({server});
-
-const sockets = [];
-
-wss.on("connection", (socket) => {
-    sockets.push(socket);
-    socket["nickname"] = "whoami";
-    console.log("Connected to Browser !");
-    socket.on("close", () => console.log("Discconected from the Browser X"))
-    socket.on("message", (msg) => {
-        const message = JSON.parse(msg);
-        switch (message.type) {
-            case "new_message":
-                sockets.forEach((aSocket) => {
-                    aSocket.send(`${socket.nickname}: ${message.payload.toString('utf-8')}`)
-                })
-                break;
-            case "nickname":
-                socket["nickname"] = message.payload;
-                break;
-            default:
-                break;
-        }
-    });
-});
+wsServer.on("connection", (socket) => {
+    socket.on("enter_room", (roomName, done) => {
+        console.log(roomName);
+        setTimeout(() => {
+            done("hello from backend");
+        }, 1000);
+    })
+})
 
 
-server.listen(3000, handleListen);
+//#region websocket script
+// import http from "http";
+// import WebSocket from "ws";
+// import express from "express";
+
+// const app = express();
+
+// app.set("view engine", "pug");
+// app.set("views", __dirname + "/views");
+// app.use("/public", express.static(__dirname + "/public"));
+// app.get("/", (_, res) => res.render("home"));
+// app.get("/*", (_, res) => res.redirect("/"));
+
+// const handleListen = () => console.log(`Listening on http://localhost:3000`);
+
+// const server = http.createServer(app);
+// const wss = new WebSocket.Server({server});
+
+// const sockets = [];
+
+// wss.on("connection", (socket) => {
+//     sockets.push(socket);
+//     socket["nickname"] = "whoami";
+//     console.log("Connected to Browser !");
+//     socket.on("close", () => console.log("Discconected from the Browser X"))
+//     socket.on("message", (msg) => {
+//         const message = JSON.parse(msg);
+//         switch (message.type) {
+//             case "new_message":
+//                 sockets.forEach((aSocket) => {
+//                     aSocket.send(`${socket.nickname}: ${message.payload.toString('utf-8')}`)
+//                 })
+//                 break;
+//             case "nickname":
+//                 socket["nickname"] = message.payload;
+//                 break;
+//             default:
+//                 break;
+//         }
+//     });
+// });
+
+// server.listen(3000, handleListen);
+//#endregion
